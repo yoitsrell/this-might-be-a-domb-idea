40c1ebf1a72b5b9b5004f98a86b33a1b95b462a6
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-02-10 15:00:12 -0500

    Still not understanding image

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const paragraph = document.querySelector('#para');
 
 paragraph.style.color = 'lightblue'
-paragraph.style.font = '5 em'
 paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
 
-const half = document.getElementById('#para').selectedIndex = "13";
+const heading = document.querySelector('h1')
+heading.style.fontSize = '5em'
+
+const half = document.querySelector("#item-13");
 half.style.opacity = '50%'
 
-const third = document.getElementById('#para').selectedIndex = "3";
+const third = document.querySelector("#item-3");
 third.innerText = 'I say, "Hi!"'
 
 const image1 = document.querySelector("#image");
